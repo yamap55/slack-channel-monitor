96edf0c33c29ed741ee35787843e43e2f269cc37
yamap55 <1785817+yamap55@users.noreply.github.com>
2023-01-08 11:43:17 +0900

    [add] deleted and unarchived trigger

---
+++ triggers/channel_unarchived.ts
@@ -0,0 +1,17 @@
+import { Trigger } from "deno-slack-api/types.ts";
+import NotifyWorkflow from "../workflows/channel_monitor.ts";
+
+const trigger: Trigger<typeof NotifyWorkflow.definition> = {
+  type: "event",
+  name: "Channel Unarchived Trigger",
+  workflow: `#/workflows/${NotifyWorkflow.definition.callback_id}`,
+  event: { event_type: "slack#/events/channel_unarchived" },
+  inputs: {
+    channel_id: { value: "{{data.channel_id}}" },
+    user_id: { value: "{{data.user_id}}" },
+    event_type: { value: "channel_unarchived" },
+    channel_type: { value: "{{data.channel_type}}" },
+  },
+};
+
+export default trigger;
